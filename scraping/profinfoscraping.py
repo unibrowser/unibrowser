b61e9a8d99d26b98c04f66dcec88e0be05d0dc93
sudharkj <sudha.rkj@gmail.com>
2018-10-29 15:20:11 -0700

    Fix name scraping and allow adding constants in research.

--- scraping/profinfoscraping.py
+++ scraping/profinfoscraping.py
@@ -19,17 +19,31 @@ def get_html(url):
     return soup
 
 
+def get_name(cur_soup_prof, name_key):
+    prof_name_list = cur_soup_prof.select(name_key['select'])
+    prof_full_name_list = []
+    for prof_name in prof_name_list:
+        prof_full_name_list = [prof_name.getText().strip()] + prof_full_name_list
+    return " ".join(prof_full_name_list)
+
+
+def get_research(cur_soup_prof, research_key):
+    if 'value' in research_key:
+        return research_key['value']
+    return cur_soup_prof.select(research_key['select'])[0].getText().strip()
+
+
 def scrape_prof_data(soup, data_structure):
     soup_prof_list = soup.select(data_structure['list']['select'])
-    prof_list = []
+    cur_prof_list = []
     for soup_prof in soup_prof_list:
         prof = {
-            'name': soup_prof.select(data_structure['name']['select'])[0].getText().strip(),
-            'research': soup_prof.select(data_structure['research']['select'])[0].getText().strip(),
+            'name': get_name(soup_prof, data_structure['name']),
+            'research': get_research(soup_prof, data_structure['research']),
             'contact': soup_prof.select(data_structure['contact']['select'])[0].getText().strip()
         }
-        prof_list.append(prof)
-    return prof_list
+        cur_prof_list.append(prof)
+    return cur_prof_list
 
 
 def save_prof_data(professors):
@@ -45,6 +59,9 @@ def save_prof_data(professors):
 
 
 if __name__ == '__main__':
+    # links to be scraped:
+    # 1. https://fa.oregonstate.edu/ambc/directory - Agricultural Sciences and Marine Sciences Business Center
+    # 2. https://anrs.oregonstate.edu/ - Animal and Rangeland
     with open('config/prof_input_structure.json', 'r') as f:
         jsonList = json.load(f)
 
