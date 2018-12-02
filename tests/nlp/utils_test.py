7f3b407d3469ee2dffa72d3917af1e42100b71de
Ravi <sudha.rkj@gmail.com>
2018-12-02 15:19:36 -0800

    Generate word clusters for faqs

--- tests/nlp/utils_test.py
+++ tests/nlp/utils_test.py
@@ -3,19 +3,34 @@ import sys
 import os
 # unit testing
 import unittest
-from pymongo import MongoClient
 
 # Sets the execution path
 sys.path.insert(0, os.path.realpath('./'))
 
 # internal modulesunit
-from nlp.utils import get_word_clusters, lemmatize_text, get_question_data
+from nlp.utils import get_faq_data, get_lemmatize_dict, get_word_clusters, save_faq_slots
 
 
 class TestUtils(unittest.TestCase):
     """
     Class containing all the unit tests for the utility functions.
     """
+
+    def test_get_faq_data(self):
+        """
+        Tests if the data is being fetched from the database.
+        """
+        faqs = get_faq_data('faq')
+        self.assertTrue(len(faqs) > 0)
+
+    def test_lemmatize_text(self):
+        """
+        Tests if the lemmatize_dict function works as expected.
+        """
+        sentences = ["testing funny dogs", "doing natural language processing", "working on computer processing"]
+        lemmatize_text_dict = get_lemmatize_dict(sentences)
+        self.assertTrue(len(lemmatize_text_dict.items()) > 0)
+
     def test_get_word_clusters(self):
         """
         Tests if the get_word_clusters function works as expected.
@@ -30,26 +45,17 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(len(lemma_slot.items()) == len(lemma_dict.items()))
         self.assertTrue(len(clusters) > 0)
 
+    def test_save_results(self):
+        thres = 0.5
+        lemma_dict = \
+            {
+                'cat': ['cats', 'cat'],
+                'dog': ['dogs', 'dog']
+            }
+        lemma_slot, clusters = get_word_clusters(lemma_dict, thres)
+        res = save_faq_slots(lemma_slot, clusters)
+        self.assertTrue(res == 1)
 
-    def test_lemmatize_text(self):
-        """
-        Tests if the lemmatize_text function works as expected.
-        """
-        noun_phrases = \
-            ["funny dogs","natural language processing","computer processing"]
-        lemma_text = lemmatize_text(noun_phrases)
-        self.assertTrue(lemma_text.__contains__("dogs"))
-        self.assertTrue(lemma_text.__contains__("processing"))
-        self.assertFalse(lemma_text.__contains__("cat"))
-        self.assertTrue(len(lemma_text) > 0)
-
-    def test_get_question_data(self):
-        """
-        Tests if the data is being fetched from the database.
-        """
-        question, answer = get_question_data('faq')
-        self.assertTrue(len(question) > 0)
-        self.assertTrue(len(answer) > 0)
 
 if __name__ == '__main__':
     """
